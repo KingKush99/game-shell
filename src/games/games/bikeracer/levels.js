26add3d333d5685751565828fcbb0bc58c7b6554
KingKush99 <92454981+KingKush99@users.noreply.github.com>
2025-08-16 06:01:05 -0400

    Delete src/games/games/bikeracer directory

--- src/games/games/bikeracer/levels.js
+++
@@ -1,5 +0,0 @@
-export const LEVELS = [
-  { name: "Sunny Hills", finishX: 5000, seed: 12345 },
-  { name: "Cliff Run",   finishX: 6500, seed: 98765 },
-  { name: "Sprint",      finishX: 3000, seed: 55555 }
-];
